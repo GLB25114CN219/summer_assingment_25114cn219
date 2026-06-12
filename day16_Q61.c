4413adaa521960227e0e21d18c48c65f159d49fe
GLB25114CN219 <vishnu821810@gmail.com>
2026-06-12 21:50:09 +0530

    Add files via upload

---
+++ day16_Q61.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0, total = 0, a[100];
+
+    printf("Enter value of n (1 to n): ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n - 1);
+    for(i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    total = n * (n + 1) / 2;
+
+    printf("Missing Number = %d", total - sum);
+
+    return 0;
+}
---
+++ day16_Q63.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, j, sum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+
+    printf("Pairs are:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] + a[j] == sum)
+                printf("(%d, %d)\n", a[i], a[j]);
+        }
+    }
+
+    return 0;
+}
